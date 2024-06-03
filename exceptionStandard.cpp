4e2089587e6b91a86cd738f4188074b603b74d5f
LENOVO <MeutyaAzzahrani>
2024-06-03 15:47:03 +0700

    Membuat penanda untuk codingan berakhir

--- exceptionStandard.cpp
+++ exceptionStandard.cpp
@@ -18,4 +18,8 @@ int main()
         cout << e.what() << endl;
         /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
     }
+    cout << "Baris Program yang terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti*/
+
+    return 0;
 }
