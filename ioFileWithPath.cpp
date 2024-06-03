d92a6b10797229cf1871eb86cdeeb12488282351
LENOVO <MeutyaAzzahrani>
2024-06-03 15:25:26 +0700

    menuliskan dan memasukan karakter q dan nilai  dari baris kedalam file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -21,5 +21,35 @@ int main() {
     while (true) {
         cout << "- ";
         //menapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menuliskan dan memasukan nilai dari 'baris' kedalam file
+        outfile << baris << endl;
     }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk kesebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file ";
+    return 0;
 }
